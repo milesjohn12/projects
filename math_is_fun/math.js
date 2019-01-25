742509de44b5abf50289a2230e4763875286d743
unknown <milesjoh001@warren.k12.in.us>
2019-01-25 13:43:34 -0500

    initial push

---
+++ math_is_fun/math.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+
+    $("#submit1").click(function(){
+        submit1();
+    });
+
+    $("#submit2").click(function(){
+        submit2();
+    });
+
+    $("#submit3").click(function(){
+        submit3();
+    });
+
+    $("#submit4").click(function(){
+        submit4();
+    });
+});
+
+
+
